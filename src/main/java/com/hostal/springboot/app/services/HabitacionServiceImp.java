ef812a17e4dbae3a012a7f6890b9b3c701b0f0db
sidgim <gaston.lara.p@gmail.com>
2019-06-13 15:34:33 -0400

    Actualizacion y validacion de algunos campos

--- src/main/java/com/hostal/springboot/app/services/HabitacionServiceImp.java
+++ src/main/java/com/hostal/springboot/app/services/HabitacionServiceImp.java
@@ -1,6 +1,7 @@
 package com.hostal.springboot.app.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
